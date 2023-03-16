9ad6b0fb16ec2b4401e5ad5983f6b93083b57c2a
DuyPNC1022I1 <duypn1408@gmail.com>
2023-03-17 01:05:59 +0700

    Update ver.09

--- src/main/java/code/gym/webmediaplayer/repository/AccountRepository.java
+++ src/main/java/code/gym/webmediaplayer/repository/AccountRepository.java
@@ -1,13 +1,9 @@
 package code.gym.webmediaplayer.repository;
 
 import code.gym.webmediaplayer.model.Account;
-import code.gym.webmediaplayer.model.Song;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Page<Account> findAllByNameContaining(String name, Pageable pageable);
 }
--- src/main/java/code/gym/webmediaplayer/service/AccountService.java
+++ src/main/java/code/gym/webmediaplayer/service/AccountService.java
@@ -42,7 +42,7 @@ public class AccountService implements Icrud<Account> {
 
     @Override
     public Page<Account> findAllByNameContaining(String name, Pageable pageable) {
-        return accountRepository.findAllByNameContaining(name, pageable);
+        return null;
     }
 
     @Override
